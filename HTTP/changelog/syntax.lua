1b71b02a6e8223a316f63fce4f74d1b8387ffac9
Dungoler <gabrielgut0509@gmail.com>
2024-07-05 17:32:39 -0400

    syntax

---
+++ HTTP/changelog/syntax.lua
@@ -0,0 +1,23 @@
+--[[ SYNTAX
+
+Update types:
+
+1: Update
+2: Patch
+3: Minor update
+
+~notices:
+
+4: Incoming update
+5: Warning
+6: Message
+
+JSON:
+[
+  1,                          -- Update type
+  "1.0.0",                    -- Title
+  "Mainly UI overhauls",      -- Content
+  "Added new <example>"       -- Elements, at least one if it isn't a notice
+  ]
+
+]]
